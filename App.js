78d0bb75bd7f3137fef5798fdf55e5ef0fda739f
dhiyo7 <dhiyoreksa12@gmail.com>
2019-11-17 18:18:08 +0700

    add Bottom Nav

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import Home from './Home';
 import Profile from './Profile';
 import Feed from './Feed';
 
-const FirstNavGroup = createSwitchNavigator({
+const FirstNavGroup = createBottomTabNavigator({
 
   HomeScreen:{
     screen: Home,
